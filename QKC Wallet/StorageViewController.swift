460e053b83b62902a2082d70268b56df43e79ba8
叶航宇 <578890471@qq.com>
2018-07-27 04:07:13 +0100

    Second View Added

---
+++ QKC Wallet/StorageViewController.swift
@@ -0,0 +1,28 @@
+//
+//  StorageViewController.swift
+//  QKC Wallet
+//
+//  Created by Jerry on 2018/7/27.
+//  Copyright © 2018年 SoftChain Foundation Ltd. All rights reserved.
+//
+
+import UIKit
+
+class StorageViewController: UIViewController {
+
+    @IBOutlet weak var privateKeyTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
