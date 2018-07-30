f0e035bbaed6b13c4b2dd62972f1ca76e0b00c9d
叶航宇 <578890471@qq.com>
2018-07-30 20:28:46 +0100

    Basic func added to storage view

--- QKC Wallet/StorageViewController.swift
+++ QKC Wallet/StorageViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class StorageViewController: UIViewController {
 
-    @IBOutlet weak var privateKeyTextView: UITextView!
+    @IBOutlet weak var privateKey1: UILabel!
+    @IBOutlet weak var privateKey2: UILabel!
+    @IBOutlet weak var privateKey3: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +25,29 @@ class StorageViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-
+    @IBAction func firstCopyPressed(_ sender: Any) {
+        UIPasteboard.general.string = privateKey1.text
+    }
+    
+    @IBAction func firstPastePressed(_ sender: Any) {
+        privateKey1.text = UIPasteboard.general.string
+    }
+    
+    @IBAction func secondCopyPressed(_ sender: Any) {
+        UIPasteboard.general.string = privateKey2.text
+    }
+    
+    @IBAction func secondPastePressed(_ sender: Any) {
+        privateKey2.text = UIPasteboard.general.string
+    }
+    
+    @IBAction func thirdCopyPressed(_ sender: Any) {
+        UIPasteboard.general.string = privateKey3.text
+    }
+    
+    @IBAction func thirdPastePressed(_ sender: Any) {
+        privateKey3.text = UIPasteboard.general.string
+        
+    }
+    
 }
